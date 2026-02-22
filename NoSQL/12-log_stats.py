eac9b57df7036761d54eb47a5f821d15dec023f2
Seyiranky <169037147+Seyiranky@users.noreply.github.com>
2026-02-22 21:37:43 +0200

    Refactor log_stats.py for improved readability

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -3,32 +3,21 @@
 from pymongo import MongoClient
 
 
-def main(collection):
-    """Print statistics about nginx logs"""
-
-    # Total number of logs
-    num_logs = collection.count_documents({})
-    print("{} logs".format(num_logs))
-
-    print("Methods:")
-
-    # List of HTTP methods
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-
-    # Count logs per method
-    for method in methods:
-        count = collection.count_documents({"method": method})
-        print("\tmethod {}: {}".format(method, count))
-
-    # Count GET requests to /status
-    status_count = collection.count_documents(
-        {"method": "GET", "path": "/status"}
-    )
-    print("{} status check".format(status_count))
-
-
 if __name__ == "__main__":
     client = MongoClient()
     db = client.logs
     collection = db.nginx
-    main(collection)
+
+    print("{} logs".format(collection.count_documents({})))
+    print("Methods:")
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        print("\tmethod {}: {}".format(
+            method,
+            collection.count_documents({"method": method})
+        ))
+
+    print("{} status check".format(
+        collection.count_documents({"method": "GET", "path": "/status"})
+    ))
